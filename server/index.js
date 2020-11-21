7e4b74b100f47cde0e30c126f3f7300aceea9464
dossmedaziz <dossaziz4@gmail.com>
2020-11-21 11:32:24 +0100

    update

--- server/index.js
+++ server/index.js
@@ -5,60 +5,66 @@ const mongoose = require("mongoose");
 const FriendModel = require("./models/Friends");
 app.use(cors())
 app.use(express.json())
-// / DATABASE CONNECTION
-// mongoose.connect(
-//   "mongodb://localhost:27017/crud?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
-//   { useNewUrlParser: true }
-// );
+/ DATABASE CONNECTION
 mongoose.connect(
-  "mongodb+srv://dossmedaziz:55208998aa@newcluster.ophcf.mongodb.net/crud?retryWrites=true&w=majority",
+  "mongodb://localhost:27017/crud?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
   { useNewUrlParser: true }
 );
 
 
-app.post("/insert", async (req, res) => {
 
-  const name = req.body.name
-  const age = req.body.age
-  const friend = new FriendModel({ name: name, age: age });
-  await friend.save();
-  res.send(friend);
-});
+          app.post("/insert", async (req, res) => {
 
-          app.get("/read", async (req, res) => {
-            FriendModel.find({}, (err, result) => {
-              if (err) {
-                res.send(err);
-              } else {
-                res.send(result);
-              }
-            });
+                        const name = req.body.name
+                        const age = req.body.age
+                        const friend = new FriendModel({ name: name, age: age });
+                        await friend.save();
+                        res.send(friend);
           });
 
-app.put('/update', async (req, res) =>{
-  const newAge = req.body.newAge
-  const id = req.body.id
-  try{
-        await FriendModel.findById(id,(error,friendToUpdate )=>{
-          friendToUpdate.age = Number(newAge);
-          friendToUpdate.save();
-        })
-  } catch(err){
-    console.log(err)
-  }
-    res.send("updated")
-}) ;
+          app.get("/read", async (req, res) => {
+                  FriendModel.find({}, (err, result) => {
+                    if (err) {
+                      res.send(err);
+                    } else {
+                      res.send(result);
+                    }
+                  });
+          });
+
+        app.put('/update', async (req, res) =>{
+
+                  const newAge = req.body.newAge
+                  const id = req.body.id
+                  try{
+                        await FriendModel.findById(id,(error,friendToUpdate )=>{
+                          friendToUpdate.age = Number(newAge);
+                          friendToUpdate.save();
+                        })
+                  } catch(err){
+                    console.log(err)
+                  }
+                res.send("updated")
+        
+              }) ;
 
 
-app.delete('/delete/:id', async (req, res) =>{
-          const id = req.params.id
-          await FriendModel.findByIdAndRemove(id).exec()
-          res.send('item deleted')
-})
 
-app.get('/',(req,res)=>{
-    res.send("it work!")
-})
+        app.delete('/delete/:id', async (req, res) =>{
+
+                      const id = req.params.id
+                      await FriendModel.findByIdAndRemove(id).exec()
+                      res.send('item deleted')
+                    
+          })
+*
+
+
+
+
+    app.get('/',(req,res)=>{
+        res.send("it work!")
+    })
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("You are connected!");
